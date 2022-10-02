65d9aa483a27b5920609a6f674930099f32d3abf
Ethan Mick <ethan@ethanmick.com>
2022-10-02 19:44:57 -0400

    Add Prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import prisma from '../lib/prisma'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -13,3 +15,17 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: 'example@test.com'
+    }
+  })
+
+  return {
+    props: {
+      user
+    }
+  }
+}
---
+++ types.d.ts
@@ -0,0 +1,3 @@
+export declare global {
+  var prisma: PrismaClient | undefined
+}
